239d26fe667c3e8a88d6f0e7dc03d8e8ad506d87
oknkc8 <oknkc8@gmail.com>
2019-05-16 09:59:50 +0900

    assignment 2 init

--- class_asignment_2/viewer.py
+++ class_asignment_2/viewer.py
@@ -16,10 +16,10 @@ g_fViewDistance = 9.
 g_Width = 600
 g_Height = 600
 
-g_nearPlane = 1.
+g_nearPlane = 30
 g_farPlane = 1000.
 
-zoom = 10.
+zoom = 50.
 
 flag_left_press = False
 flag_right_press = False
@@ -48,6 +48,116 @@ def drawXZ():
         glVertex3fv(np.array([5,0,i]))
         glVertex3fv(np.array([-5,0,i]))
     glEnd()
+
+def drawSphere(numLats=12, numLongs=12, size=1, x_ratio=1, y_ratio=1, z_ratio=1):
+    for i in range(0, numLats + 1):
+        lat0 = np.pi * (-0.5 + float(float(i - 1) / float(numLats)))
+        z0 = np.sin(lat0) * size
+        zr0 = np.cos(lat0) * size
+
+        lat1 = np.pi * (-0.5 + float(float(i) / float(numLats)))
+        z1 = np.sin(lat1) * size
+        zr1 = np.cos(lat1) * size
+
+        # Use Quad strips to draw the sphere
+        glBegin(GL_QUAD_STRIP)
+
+        for j in range(0, numLongs + 1):
+            lng = 2 * np.pi * float(float(j - 1) / float(numLongs))
+            x = np.cos(lng) * x_ratio
+            y = np.sin(lng) * y_ratio
+            glVertex3f(x * zr0, y * zr0, z0 * z_ratio)
+            glVertex3f(x * zr1, y * zr1, z1 * z_ratio)
+        
+        glEnd()
+
+def drawRunner():
+    t = glfw.get_time()
+    glColor3ub(255, 255, 255)
+    # draw body
+    glPushMatrix()
+    glTranslatef(0,np.sin(7*t)*0.1,0)
+    drawSphere(30,30,2,.3,.8,.3)
+    
+    # draw head
+    glPushMatrix()
+    glTranslatef(0,2,0)
+    drawSphere(30,30,.5, 1, 1, 1)
+    glPopMatrix()
+
+    # draw arm right-1
+    glPushMatrix()
+    glTranslatef(0, 1, .63)
+    glRotatef(-30,1,0,0)
+    glRotate(-np.sin(7*t)*50,0,0,1)
+    glTranslate(0,-.6,0)
+    drawSphere(10,10,.6,.25,1.3,.25)
+
+    #draw arm right-2
+    glPushMatrix()
+    glTranslatef(0,-.6,0)
+    glRotate(30,1,0,0)
+    glRotate(-np.sin(7*t)*45+45,0,0,1)
+    glTranslate(0,-.4,0)
+    drawSphere(10,10,.4,.25,1.3,.25)
+    glPopMatrix()
+
+    glPopMatrix()
+
+    # draw arm left-1
+    glPushMatrix()
+    glTranslatef(0, 1, -.63)
+    glRotatef(30,1,0,0)
+    glRotate(np.sin(7*t)*50,0,0,1)
+    glTranslate(0,-.6,0)
+    drawSphere(10,10,.6,.25,1.3,.25)
+
+    #draw arm left-2
+    glPushMatrix()
+    glTranslatef(0,-.6,0)
+    glRotate(-30,1,0,0)
+    glRotate(np.sin(7*t)*45+45,0,0,1)
+    glTranslate(0,-.4,0)
+    drawSphere(10,10,.4,.25,1.3,.25)
+    glPopMatrix()
+
+    glPopMatrix()
+
+    # draw leg right-1
+    glPushMatrix()
+    glTranslatef(0, -1, .43)
+    glRotate(np.sin(7*t)*45,0,0,1)
+    glTranslate(0,-.6,0)
+    drawSphere(10,10,.7,.25,1,.25)
+
+    # draw leg right-2
+    glPushMatrix()
+    glTranslatef(0,-.7,0)
+    glRotate(np.sin(7*t)*45-45,0,0,1)
+    glTranslate(0,-.4,0)
+    drawSphere(10,10,.4,.4,1.3,.4)
+    glPopMatrix()
+
+    glPopMatrix()
+
+    # draw leg left-1
+    glPushMatrix()
+    glTranslatef(0, -1, -.43)
+    glRotate(-np.sin(7*t)*45,0,0,1)
+    glTranslate(0,-.6,0)
+    drawSphere(10,10,.7,.25,1,.25)
+
+    # draw leg left-2
+    glPushMatrix()
+    glTranslatef(0,-.7,0)
+    glRotate(-np.sin(7*t)*45-45,0,0,1)
+    glTranslate(0,-.4,0)
+    drawSphere(10,10,.4,.4,1.3,.4)
+    glPopMatrix()
+
+    glPopMatrix()
+
+    glPopMatrix()
 
 def cursor_callback(window, xpos, ypos):
     global x_rotate, y_rotate
@@ -88,25 +198,23 @@ def render():
     glPolygonMode( GL_FRONT_AND_BACK, GL_LINE )     
     glLoadIdentity() 
     gluLookAt(0, 0, -g_fViewDistance, 0, 0, 0, 0., .1, 0)
-	# Set perspective (also zoom)
+	
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     gluPerspective(zoom, float(g_Width)/float(g_Height), g_nearPlane, g_farPlane)
+    glTranslatef(0., 0., -30)
     glMatrixMode(GL_MODELVIEW)
 
-    #glLoadIdentity()
-    #glPushMatrix()
-    glTranslatef(scene_x_pos/500., 0., 0.)
-    glTranslatef(0., scene_y_pos/500., 0.)
+    glTranslatef(scene_x_pos/100., 0., 0.)
+    glTranslatef(0., scene_y_pos/100., 0.)
 
-    #glPopMatrix()
     glRotatef(y_rotate/2., 1., 0., 0.)
-    glRotatef(x_rotate/2., 0., 1., 0.)
-    
+    glRotatef(x_rotate/2., 0., 1., 0.)    
 
     drawFrame()
     drawXZ()
-    #drawCube()
+
+    drawRunner()
 
 def size_callback(window, width, height):
     global g_Width, g_Height
